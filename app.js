6b0977717e4183252ce16e05e4cb28a5ef74d360
Logan Willaumez <Logan.willaumez@gmail.com>
2021-11-04 15:53:49 +0100

    add js form

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-function test() {
+const btn = document.querySelector('button')
+const form = document.querySelector('form')
+const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+btn.addEventListener('click', valid =>{
+let input = document.querySelector('input').value;
+   if (!input.match(regExp) ){
+        form.classList.add('active')
+        form.classList.remove('bounce')
+        void form.offsetWidth
+       form.classList.add('bounce')
+    }
+    else{
+        form.classList.remove('active')
+    }
+   
+  
+})
 
-    let test = 1;
-}
-
-
-
-
-
-
-
-
-
-
-test+=1;
